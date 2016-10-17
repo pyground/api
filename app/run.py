e0975d51c09c95e05bc69c5e44597b34c2125ac5
Guilherme Diego <guidiego.expgames@gmail.com>
2016-10-16 22:48:29 -0200

    Added tornado resource

---
+++ app/run.py
@@ -0,0 +1,8 @@
+# TODO: Routes <3
+# import tornado.ioloop
+#
+# app = web.Application(routes, debug=True)
+#
+# if __name__ == '__main__':
+#     app.listen(8001)
+#     tornado.ioloop.IOLoop.instance().start()
---
+++ app/user/resources.py
@@ -0,0 +1,6 @@
+from utils.generic_resource import GenericResource
+from user.models import User
+
+class UserResource(GenericResource):
+    def __init__(self)
+        super().__init__(User)
