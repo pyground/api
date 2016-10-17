1d3128239d7e8600bad4c4c62d79f65cd8b2953c
Guilherme Diego <guidiego.expgames@gmail.com>
2016-10-17 13:41:22 -0200

    Refactor core to another lib

--- app/snippet/resources.py
+++ app/snippet/resources.py
@@ -1,8 +1,20 @@
 from utils.generic_resource import GenericResource
 from snippet.models import Snippet
+from pynterpreter import Pynterpreter
+from tornado import gen
 
 class SnippetResource(GenericResource):
-    def __init__(self)
+    def __init__(self):
         super().__init__(Snippet)
 
-snippet_routes = router_build(User, "snippet")
+    @gen.coroutine
+    def run_code(self):
+        pntr = Pynterpreter()
+
+        raise gen.Return({
+            "response": pntr.run_as_module(self.request.GET["code"])
+        })
+
+snippet_additional_route = (r'api/snippet/run', SnippetResource.as_detail(), SnippetResource.run_code);
+
+snippet_routes = router_build(User, "snippet").append(snippet_additional_route)
